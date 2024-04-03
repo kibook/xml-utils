7290c20347710f8962c0fd522dbae9661720be0c
kibook <kibuzhj@gmail.com>
2024-04-03 15:58:03 +0000

    Fix build-mingw.sh script

--- build-mingw.sh
+++ build-mingw.sh
@@ -5,4 +5,4 @@ out=build/$MSYSTEM
 make
 mkdir -p "$out"
 cp utils/xml-*/xml-*.exe "$out"
-ldd "$out"/xml-*.exe | awk '{print $3}' | grep '^/mingw64' | sort -u | xargs cp -t "$out"
+ldd "$out"/xml-*.exe | awk '{print $3}' | grep '^/mingw' | sort -u | xargs cp -t "$out"
