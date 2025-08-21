73073413181f18ebb74a43fb7061c3cce7d6fd29
kibook <kibuzhj@gmail.com>
2025-08-21 10:11:56 -0400

    Use parallel make in build-mingw.sh

--- build-mingw.sh
+++ build-mingw.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+set -e
+
 out=build/$MSYSTEM
 
-make
 mkdir -p "$out"
+make -j$(nproc) clean
+make -j$(nproc) all
 cp utils/xml-*/xml-*.exe "$out"
 ldd "$out"/xml-*.exe | awk '{print $3}' | grep '^/mingw' | sort -u | xargs cp -t "$out"
